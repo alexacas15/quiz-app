bffd44fc276c2b827c8d1594ae10874f5d96887c
alexacas15 <alexaplecercas@gmail.com>
2025-02-18 16:14:26 +0800

    Add files via upload

---
+++ App.jsx
@@ -0,0 +1,20 @@
+import QuizApp from "./components/Quiz.jsx";
+function App() {
+  const [count, setCount] = useState(0)
+
+  return (
+  <>
+  <Quiz-App
+      City1 = "Berlin"
+      City2 = "Madrid"
+      City3 = "Paris"
+      City4 = "Lisbon"
+  />
+  </>
+  
+  
+
+  )
+}
+
+export default App
